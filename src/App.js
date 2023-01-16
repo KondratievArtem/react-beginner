67d6ddd40d97c1015270d28d5d7fd6c8063f8f5d
KondratievArtem <kondrativ41@gmail.com>
2023-01-16 16:31:31 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,60 +1,70 @@
+import React from 'react';
 import './index.scss';
 
-const questions = [
-  {
-    title: 'React - это ... ?',
-    variants: ['библиотека', 'фреймворк', 'приложение'],
-    correct: 0,
-  },
-  {
-    title: 'Компонент - это ... ',
-    variants: ['приложение', 'часть приложения или страницы', 'то, что я не знаю что такое'],
-    correct: 1,
-  },
-  {
-    title: 'Что такое JSX?',
-    variants: [
-      'Это простой HTML',
-      'Это функция',
-      'Это тот же HTML, но с возможностью выполнять JS-код',
-    ],
-    correct: 2,
-  },
-];
+import questions from './questions';
 
-function Result() {
-  return (
-    <div className="result">
-      <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
-      <h2>Вы отгадали 3 ответа из 10</h2>
-      <button>Попробовать снова</button>
-    </div>
-  );
+function Result({ correct, setLvl, setRender, setCorrect }) {
+	const restart = () => {
+		setLvl(0);
+		setCorrect(0);
+		setRender(true);
+	};
+	return (
+		<div className="result">
+			<img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
+			<h2>
+				Вы отгадали {correct} ответа из {questions.length}
+			</h2>
+			<button onClick={() => restart()}>Попробовать снова</button>
+		</div>
+	);
 }
 
-function Game() {
-  return (
-    <>
-      <div className="progress">
-        <div style={{ width: '50%' }} className="progress__inner"></div>
-      </div>
-      <h1>Что такое useState?</h1>
-      <ul>
-        <li>Это функция для хранения данных компонента</li>
-        <li>Это глобальный стейт</li>
-        <li>Это когда на ты никому не нужен</li>
-      </ul>
-    </>
-  );
+function Game({ lvl, setLvl, setRender, setCorrect, correct }) {
+	const progres = lvl * 33.333;
+
+	const clickVariant = (e) => {
+		setLvl(lvl + 1);
+		const ansver = e.target.innerText;
+
+		if (questions[lvl].variants.indexOf(ansver) === questions[lvl].correct) {
+			setCorrect(correct + 1);
+		}
+	};
+
+	if (lvl > questions.length - 1) {
+		return setRender(false);
+	}
+	return (
+		<>
+			<div className="progress">
+				<div style={{ width: progres + '%' }} className="progress__inner"></div>
+			</div>
+			<h1>{questions[lvl].title}</h1>
+			{questions[lvl].variants.map((obj, index) => (
+				<ul key={index}>
+					<li onClick={(e) => clickVariant(e)}>{obj}</li>
+				</ul>
+			))}
+		</>
+	);
 }
 
 function App() {
-  return (
-    <div className="App">
-      <Game />
-      {/* <Result /> */}
-    </div>
-  );
+	const [render, setRender] = React.useState(true);
+	const [lvl, setLvl] = React.useState(0);
+	const [correct, setCorrect] = React.useState(0);
+
+	React.useEffect(() => {
+		console.log(lvl);
+	}, [lvl]);
+	return (
+		<div className="App">
+			{render && <Game lvl={lvl} setLvl={setLvl} setRender={setRender} setCorrect={setCorrect} correct={correct} />}
+
+			{!render && <Result setRender={setRender} setLvl={setLvl} correct={correct} setCorrect={setCorrect} />}
+		</div>
+	);
 }
 
 export default App;
