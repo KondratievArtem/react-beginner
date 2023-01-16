df0037b549370067e99faf181c3c74d70e9702c0
KondratievArtem <kondrativ41@gmail.com>
2023-01-16 08:39:12 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,43 @@
+import React from 'react';
 import './index.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <div>
-        <h2>Счетчик:</h2>
-        <h1>0</h1>
-        <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
-      </div>
-    </div>
-  );
+	const [count, setCount] = React.useState(0);
+	const [status, setStatus] = React.useState('');
+
+	const countStatus = () => {
+		if (count === 0) {
+			setStatus('');
+		}
+		if (count < 0) {
+			setStatus('minus');
+		}
+		if (count > 0) {
+			setStatus('plus');
+		}
+	};
+
+	React.useEffect(() => {
+		countStatus();
+	}, [count]);
+
+	return (
+		<div className="App">
+			<div>
+				<h2>Лічильник:</h2>
+				<h1 className={status}>{count}</h1>
+				<button className="minus" onClick={() => setCount(count - 1)}>
+					- Мінус
+				</button>
+				<button className="reset" onClick={() => setCount(0)}>
+					Обнулкння
+				</button>
+				<button className="plus" onClick={() => setCount(count + 1)}>
+					Плюс +
+				</button>
+			</div>
+		</div>
+	);
 }
 
 export default App;
