2277970eaec6b15b077055ae8b23acc58176401f
KondratievArtem <kondrativ41@gmail.com>
2023-01-17 11:02:33 +0200

    first commit app

--- src/App.js
+++ src/App.js
@@ -6,12 +6,39 @@ import { Users } from './components/Users';
 // Тут список пользователей: https://reqres.in/api/users
 
 function App() {
-  return (
-    <div className="App">
-      <Users />
-      {/* <Success /> */}
-    </div>
-  );
+	const [getUsers, setGetUsers] = React.useState([]);
+	const [isLoading, setIsLoading] = React.useState(true);
+	const [invited, setInvited] = React.useState([]);
+	const [sendUsers, setSendUsers] = React.useState(false);
+
+	const onClickInvit = (id) => {
+		if (invited.includes(id)) {
+			setInvited((prev) => prev.filter((_id) => _id !== id));
+		} else {
+			setInvited((prev) => [...prev, id]);
+		}
+	};
+
+	React.useEffect(() => {
+		fetch('https://reqres.in/api/users')
+			.then((res) => res.json())
+			.then(({ data }) => setGetUsers(data))
+			.catch((err) => {
+				console.error('Error: ', err);
+				alert('Помилка загрузки користувачів');
+			})
+			.finally(setIsLoading(false));
+	}, []);
+
+	return (
+		<div className="App">
+			{sendUsers ? (
+				<Success count={invited.length} />
+			) : (
+				<Users setSendUsers={setSendUsers} invited={invited} onClickInvit={onClickInvit} items={getUsers} isLoading={isLoading} />
+			)}
+		</div>
+	);
 }
 
 export default App;
--- src/components/Users/index.jsx
+++ src/components/Users/index.jsx
@@ -2,27 +2,41 @@ import React from 'react';
 import { Skeleton } from './Skeleton';
 import { User } from './User';
 
-export const Users = ({ items, isLoading }) => {
-  return (
-    <>
-      <div className="search">
-        <svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-          <path d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z" />
-        </svg>
-        <input type="text" placeholder="Найти пользователя..." />
-      </div>
-      {isLoading ? (
-        <div className="skeleton-list">
-          <Skeleton />
-          <Skeleton />
-          <Skeleton />
-        </div>
-      ) : (
-        <ul className="users-list">
-          <User />
-        </ul>
-      )}
-      <button className="send-invite-btn">Отправить приглашение</button>
-    </>
-  );
+export const Users = ({ items, isLoading, onClickInvit, invited, setSendUsers }) => {
+	const [searchValue, setSearchValue] = React.useState('');
+
+	return (
+		<>
+			<div className="search">
+				<svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+					<path d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z" />
+				</svg>
+				<input onChange={(event) => setSearchValue(event.target.value.trim().toLowerCase())} type="text" placeholder="Найти пользователя..." />
+			</div>
+			{isLoading ? (
+				<div className="skeleton-list">
+					<Skeleton />
+					<Skeleton />
+					<Skeleton />
+				</div>
+			) : (
+				<ul className="users-list">
+					{items
+						.filter((obj) => {
+							const fullName = obj.first_name + ' ' + obj.last_name;
+
+							return fullName.toLowerCase().includes(searchValue) || obj.email.toLowerCase().includes(searchValue);
+						})
+						.map((obj) => (
+							<User isInvited={invited.includes(obj.id)} onClickInvit={onClickInvit} key={obj.id} {...obj} />
+						))}
+				</ul>
+			)}
+			{invited.length > 0 && (
+				<button onClick={() => setSendUsers(true)} className="send-invite-btn">
+					Отправить приглашение
+				</button>
+			)}
+		</>
+	);
 };
